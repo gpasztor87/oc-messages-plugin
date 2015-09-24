eff22d600281ddbddb302667da238f652fc90b56
Pásztor Gábor <gpasztor87@gmail.com>
2015-09-24 11:37:40 +0200

    Create and reply form

--- components/Message.php
+++ components/Message.php
@@ -4,7 +4,6 @@ use Auth;
 use Redirect;
 use Validator;
 use Carbon\Carbon;
-use Cms\Classes\Page;
 use Cms\Classes\ComponentBase;
 use Autumn\Messages\Models\Message as MessageModel;
 use Autumn\Messages\Models\MessageEntry;
@@ -30,8 +29,23 @@ class Message extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'        => 'Messages',
+            'name'        => 'Message',
             'description' => 'Display a list of message entries on the page.'
+        ];
+    }
+
+    /**
+     * Defines the properties used by this class.
+     */
+    public function defineProperties()
+    {
+        return [
+            'slug'         => [
+                'title'       => 'Slug param name',
+                'description' => 'The URL route parameter used for looking up the message by its slug.',
+                'default'     => '{{ :slug }}',
+                'type'        => 'string'
+            ]
         ];
     }
 
@@ -41,7 +55,79 @@ class Message extends ComponentBase
      */
     public function onRun()
     {
+        return $this->prepareEntryList();
+    }
 
+    /**
+     * Returns the logged in user, if available
+     */
+    public function user()
+    {
+        if (!Auth::check()) {
+            return null;
+        }
+
+        return Auth::getUser();
+    }
+
+    protected function prepareEntryList()
+    {
+        if ($message = $this->getMessage()) {
+            $this->message = $this->page['message'] = $message;
+        }
+    }
+
+    protected function getMessage()
+    {
+        $slug = $this->property('slug');
+
+        $message = MessageModel::whereSlug($slug)->first();
+        if ($message != null) {
+            $userMessage = UserMessage::where('user_id', $this->user()->id)
+                ->where('message_id', $message->id)->first();
+
+            if ($userMessage != null) {
+                $userMessage->last_viewed = Carbon::now();
+                $userMessage->save();
+
+                return $message;
+            }
+        }
+        else {
+            $message = $this->user()->messages->first();
+        }
+
+        return $message;
+    }
+
+    public function onReplyMessage()
+    {
+        if (!$user = Auth::getUser()) {
+            throw new ApplicationException('You should be logged in.');
+        }
+
+        $rules = [
+            'content' => 'required'
+        ];
+
+        $validation = Validator::make(input(), $rules);
+        if ($validation->fails()) {
+            throw new ValidationException($validation);
+        }
+
+        $message = $this->getMessage();
+
+        // Attach Message Entry
+        $messageEntry = new MessageEntry;
+        $messageEntry->user = $user;
+        $messageEntry->message = $message;
+        $messageEntry->content = input('content');
+        $messageEntry->save();
+
+        $message->updated_at = Carbon::now();
+        $message->save();
+
+        $this->prepareEntryList();
     }
 
 }
